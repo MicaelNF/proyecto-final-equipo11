79f09493e693b6751d6e4d620a759e3440dec75b
MicaelNF <micaelnf@ciencias.unam.mx>
2024-11-26 18:10:56 -0600

    Se añadió la clase User que modela una partida / usuario.

---
+++ src/Usuarios/User.java
@@ -0,0 +1,81 @@
+package src.Usuarios;
+import java.io.*;
+import java.io.Serializable;
+
+import src.Usuarios.UsuariosExcepciones.*;
+
+public class User implements Serializable {
+
+    private int saldo;
+    private String id;
+    private int puntos;
+
+    public User(String id) {
+        
+        // Se verifica que no se pueda pasar como parámetro un id nulo.
+        if (id == null) {
+            throw new IllegalArgumentException("No puedes crear un usuario nulo.");
+        }
+
+        // Se inicializa el usuario con el id que se paso como parámetro y con un saldo inicial de 100.
+        this.id = id;
+        saldo = 100;
+    }
+    
+    // Método que se encarga de regresar el saldo actual.
+    public int obtenerSaldo() {
+        return saldo;
+    }
+
+    // Método que disminuye el saldo de acuerdo al parámetro.
+    public void retirarSaldo(int saldoARetirar) throws SinSaldoSuficienteExcepcion {
+        if (saldo < saldoARetirar) {
+            throw new SinSaldoSuficienteExcepcion(" No cuentas con el saldo suficiente.");
+        }
+        saldo = saldo - saldoARetirar;
+    }
+
+    // Metodo que regresa el ID.
+    public String obtenerId() {
+        return id;
+    }
+
+    // Metodo que regresa los puntos actuales.
+    public int obtenerPuntos() {
+        return puntos;
+    }
+
+    // Metodo que suma puntos a los actuales.
+    public void sumarPuntos(int puntosAñadir) {
+        puntos = puntos + puntosAñadir;
+    }
+    // Método que crea la ruta para guardar el achivo.
+    public File obtenerRuta() {
+        String ruta = "src/Partidas/";
+        ruta += id + ".txt";
+        
+        return new File(ruta);
+    }
+
+    public String obtenerRutaString() {
+        String ruta = "src/Partidas/";
+        ruta += id + ".txt";
+        
+        return ruta;
+    }
+
+    @Override
+    public String toString(){
+        String cadena = "";
+        cadena += "\t*Nombre de usuario: " + id + "\n" +
+                "\t*Saldo actual: " + saldo + "\n" +
+                "\t*Puntuación: " + puntos;
+        
+        return cadena;
+    }
+
+    public static void main(String[] args) {
+        User usuario = new User("MICAEL");
+        System.out.println(usuario);
+    }
+}

70d78d1c01ca964d082cfbf66fdefbbc48362fa3
MicaelNF <micaelnf@ciencias.unam.mx>
2024-11-27 18:07:47 -0600

    Se agrego la documentación respectiva a el paquete Usuarios.

--- src/Usuarios/UsuariosExcepciones/SinSaldoSuficienteExcepcion.java
+++ src/Usuarios/UsuariosExcepciones/SinSaldoSuficienteExcepcion.java
@@ -1,10 +1,31 @@
+/**
+ * Clase {@code SinSaldoSuficienteExcepcion} que representa una excepción lanzada
+ * cuando un usuario intenta realizar una operación que requiere más saldo del
+ * disponible en su cuenta.
+ * 
+ * @author Nolasco Flores Micael
+ * @author Romualdo Valera Seyin Xuxek
+ * @date 27-11-2024
+ * @version 1.0
+ */
 package src.Usuarios.UsuariosExcepciones;
 
 public class SinSaldoSuficienteExcepcion extends Exception {
+
+    /**
+     * Constructor por omisión de la clase {@code SinSaldoSuficienteExcepcion}.
+     * Inicializa la excepción sin un mensaje específico.
+     */
     public SinSaldoSuficienteExcepcion() {
         super();
     }
 
+    /**
+     * Constructor por parámetros de la clase {@code SinSaldoSuficienteExcepcion}.
+     * Inicializa la excepción con un mensaje descriptivo.
+     * 
+     * @param e el mensaje que describe el motivo de la excepción.
+     */
     public SinSaldoSuficienteExcepcion(String e) {
         super(e);
     }
